8bd1304a2b2e878b6b21013339807a88886ca293
Victor Chandra <victorchandra0911@gmail.com>
2024-11-10 14:05:42 +0700

    catering package resource

--- app/Filament/Resources/CateringPackageResource.php
+++ app/Filament/Resources/CateringPackageResource.php
@@ -6,9 +6,19 @@ use App\Filament\Resources\CateringPackageResource\Pages;
 use App\Filament\Resources\CateringPackageResource\RelationManagers;
 use App\Models\CateringPackage;
 use Filament\Forms;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -26,6 +36,54 @@ class CateringPackageResource extends Resource
         return $form
             ->schema([
                 //
+                Fieldset::make('Details')
+                ->schema([
+                    TextInput::make('name')
+                    ->maxLength(255)
+                    ->required(),
+
+                    FileUpload::make('thumbnail')
+                    ->required()
+                    ->image(),
+
+                    Repeater::make('photos')
+                    ->relationship('photos')
+                    ->schema([
+                        FileUpload::make('photo')
+                        ->required(),
+                    ]),
+                ]),
+
+                Fieldset::make('Additional')
+                ->schema([
+                    Textarea::make('about')
+                    ->required(),
+
+                    Select::make('is_popular')
+                    ->options([
+                        true => 'Popular',
+                        false => 'Not Popular',
+                    ])
+                    ->required(),
+
+                    Select::make('city_id')
+                    ->relationship('city', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
+                    Select::make('kitchen_id')
+                    ->relationship('kitchen', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+
+                    Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+                ])
             ]);
     }
 
@@ -34,11 +92,34 @@ class CateringPackageResource extends Resource
         return $table
             ->columns([
                 //
+                ImageColumn::make('thumbnail'),
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('kitchen.name'), // kitchen comes from model
+                IconColumn::make('is_popular')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Popular'),
             ])
             ->filters([
+                SelectFilter::make('city_id') // column foreign key in this database
+                    ->label('City') // column table name
+                    ->relationship('city', 'name'), // city comes from model, name is the record that want to be shown
+
+                SelectFilter::make('kitchen_id')
+                    ->label('Kitchen')
+                    ->relationship('kitchen', 'name'),
+
+                SelectFilter::make('category_id')
+                    ->label('Category')
+                    ->relationship('category', 'name'),
+
                 Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
